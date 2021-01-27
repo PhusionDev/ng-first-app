f2361f7c3f54148e57d3ba0483be44882b8f72a1
Jared Hodgkins <phusiondev@gmail.com>
2021-01-27 08:48:14 -0500

    Assignment 1

---
+++ src/app/success_alert/success_alert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success_alert.component.html',
+  styleUrls: ['./success_alert.component.css']
+})
+export class SuccessAlertComponent {}
